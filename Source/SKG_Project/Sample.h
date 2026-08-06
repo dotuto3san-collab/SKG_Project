2e7f5630f7b805f277f18c4f3ce78db56538cde0
Luna* <Luna.20.06.05.27@gmail.com>
2026-08-06 20:56:53 +0900

    C++対応用の仮ファイル追加

---
+++ Source/SKG_Project/Sample.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class SKG_PROJECT_API Sample
+{
+public:
+	Sample();
+	~Sample();
+};
