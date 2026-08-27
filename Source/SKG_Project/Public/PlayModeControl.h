c743dc0ef9d3138417b70f4f13cd1413b5c8c764
Luna* <Luna.20.06.05.27@gmail.com>
2026-08-27 15:33:03 +0900

    C++ファイル追加

---
+++ Source/SKG_Project/Public/PlayModeControl.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "PlayModeControl.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SKG_PROJECT_API UPlayModeControl : public UObject
+{
+	GENERATED_BODY()
+	
+};
