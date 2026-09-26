704e01bf0882341dbd12d3277acddd90faf98753
Luna* <Luna.20.06.05.27@gmail.com>
2026-09-26 20:30:08 +0900

    ファイル追加

---
+++ Source/SKG_Project/Public/ScreenShots.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "ScreenShots.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SKG_PROJECT_API UScreenShots : public UObject
+{
+	GENERATED_BODY()
+	
+};
